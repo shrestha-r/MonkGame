d64884ce3cc6a9de95dca27fe0cdc7572adc5807
= <=>
2026-04-28 20:48:59 +0100

    feature: combat fully player-controlled

--- src/MonsterRoom.cpp
+++ src/MonsterRoom.cpp
@@ -7,28 +7,56 @@ void MonsterRoom::enter(Monk& monk) {
     std::cout << "Monster Room! A Goblin appears!\n";
 
     while (monk.isAlive() && goblin.isAlive()) {
-        int playerAction = rand() % 2;
-        int goblinAction = rand() % 2;
 
-        if (playerAction == 0 && rand()%2) {
-            goblin.takeDamage(monk.getAttack());
-            std::cout << "Monk attacks!\n";
-        } else if (playerAction == 1 && rand()%2) {
-            monk.heal(1);
-            std::cout << "Monk guards!\n";
+        int choice;
+        std::cout << "\nChoose action:\n";
+        std::cout << "1. Attack\n2. Guard\n";
+        std::cin >> choice;
+
+        bool success = rand() % 2; // 50% chance
+
+        if (choice == 1) {
+            if (success) {
+                goblin.takeDamage(monk.getAttack());
+                std::cout << "You hit the goblin!\n";
+            } else {
+                std::cout << "Attack missed!\n";
+            }
+        } 
+        else if (choice == 2) {
+            if (success) {
+                monk.heal(1);
+                std::cout << "You recover 1 HP.\n";
+            } else {
+                std::cout << "Guard failed!\n";
+            }
+        } 
+        else {
+            std::cout << "Invalid action.\n";
+            continue;
         }
 
-        if (goblinAction == 0 && rand()%2) {
+        // Goblin turn (still simple AI)
+        int gAction = rand() % 2;
+
+        if (gAction == 0 && rand() % 2) {
             monk.takeDamage(goblin.getAttack());
             std::cout << "Goblin attacks!\n";
-        } else if (goblinAction == 1 && rand()%2) {
+        } else {
             goblin.heal(1);
+            std::cout << "Goblin defends!\n";
         }
 
         std::cout << "Monk HP: " << monk.getHealth()
-                  << " Goblin HP: " << goblin.getHealth() << "\n";
+                  << " | Goblin HP: " << goblin.getHealth() << "\n";
     }
+
+    if (!monk.isAlive())
+        std::cout << "You died in battle.\n";
+    else
+        std::cout << "Goblin defeated!\n";
 }
+
 std::string MonsterRoom::getType() const {
     return "Monster";
 }
