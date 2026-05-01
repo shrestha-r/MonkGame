5252bf212951c55361bab285a756edefc7f55ab7
= <=>
2026-05-01 07:24:28 +0100

    features improve

--- build/CMakeFiles/MonkGame.dir/main.cpp.o.d
+++ build/CMakeFiles/MonkGame.dir/main.cpp.o.d
@@ -180,4 +180,39 @@ CMakeFiles/MonkGame.dir/main.cpp.o: /home/r-sh/Desktop/MonkGame/main.cpp \
  /usr/include/c++/15.2.0/bits/locale_facets.tcc \
  /usr/include/c++/15.2.0/bits/basic_ios.tcc \
  /usr/include/c++/15.2.0/bits/ostream.tcc /usr/include/c++/15.2.0/istream \
- /usr/include/c++/15.2.0/bits/istream.tcc /usr/include/c++/15.2.0/limits
+ /usr/include/c++/15.2.0/bits/istream.tcc /usr/include/c++/15.2.0/limits \
+ /usr/include/c++/15.2.0/random /usr/include/c++/15.2.0/cmath \
+ /usr/include/math.h /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-macros.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-narrow.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h \
+ /usr/include/c++/15.2.0/bits/specfun.h \
+ /usr/include/c++/15.2.0/tr1/gamma.tcc \
+ /usr/include/c++/15.2.0/tr1/special_function_util.h \
+ /usr/include/c++/15.2.0/tr1/bessel_function.tcc \
+ /usr/include/c++/15.2.0/tr1/beta_function.tcc \
+ /usr/include/c++/15.2.0/tr1/ell_integral.tcc \
+ /usr/include/c++/15.2.0/tr1/exp_integral.tcc \
+ /usr/include/c++/15.2.0/tr1/hypergeometric.tcc \
+ /usr/include/c++/15.2.0/tr1/legendre_function.tcc \
+ /usr/include/c++/15.2.0/tr1/modified_bessel_func.tcc \
+ /usr/include/c++/15.2.0/tr1/poly_hermite.tcc \
+ /usr/include/c++/15.2.0/tr1/poly_laguerre.tcc \
+ /usr/include/c++/15.2.0/tr1/riemann_zeta.tcc \
+ /usr/include/c++/15.2.0/cstdint \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/15.2.0/include/stdint.h \
+ /usr/include/stdint.h /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /usr/include/c++/15.2.0/bits/random.h \
+ /usr/include/c++/15.2.0/bits/uniform_int_dist.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/opt_random.h \
+ /usr/include/c++/15.2.0/bits/random.tcc /usr/include/c++/15.2.0/numeric \
+ /usr/include/c++/15.2.0/bits/stl_numeric.h \
+ /usr/include/c++/15.2.0/pstl/glue_numeric_defs.h \
+ /usr/include/c++/15.2.0/pstl/execution_defs.h
--- build/CMakeFiles/MonkGame.dir/src/Dungeon.cpp.o.d
+++ build/CMakeFiles/MonkGame.dir/src/Dungeon.cpp.o.d
@@ -176,4 +176,50 @@ CMakeFiles/MonkGame.dir/src/Dungeon.cpp.o: \
  /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/opt_random.h \
  /usr/include/c++/15.2.0/bits/random.tcc /usr/include/c++/15.2.0/numeric \
  /usr/include/c++/15.2.0/bits/stl_numeric.h \
- /usr/include/c++/15.2.0/pstl/glue_numeric_defs.h
+ /usr/include/c++/15.2.0/pstl/glue_numeric_defs.h \
+ /usr/include/c++/15.2.0/iostream /usr/include/c++/15.2.0/ostream \
+ /usr/include/c++/15.2.0/bits/ostream.h /usr/include/c++/15.2.0/ios \
+ /usr/include/c++/15.2.0/exception \
+ /usr/include/c++/15.2.0/bits/exception_ptr.h \
+ /usr/include/c++/15.2.0/bits/cxxabi_init_exception.h \
+ /usr/include/c++/15.2.0/typeinfo \
+ /usr/include/c++/15.2.0/bits/nested_exception.h \
+ /usr/include/c++/15.2.0/bits/ios_base.h \
+ /usr/include/c++/15.2.0/ext/atomicity.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/gthr.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/gthr-default.h \
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/15.2.0/include-fixed/pthread.h \
+ /usr/include/sched.h /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/linux/sched/types.h /usr/include/linux/types.h \
+ /usr/include/x86_64-linux-gnu/asm/types.h \
+ /usr/include/asm-generic/types.h /usr/include/asm-generic/int-ll64.h \
+ /usr/include/x86_64-linux-gnu/asm/bitsperlong.h \
+ /usr/include/asm-generic/bitsperlong.h /usr/include/linux/posix_types.h \
+ /usr/include/linux/stddef.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
+ /usr/include/asm-generic/posix_types.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_sched_param.h \
+ /usr/include/x86_64-linux-gnu/bits/cpu-set.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct___jmp_buf_tag.h \
+ /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/atomic_word.h \
+ /usr/include/x86_64-linux-gnu/sys/single_threaded.h \
+ /usr/include/c++/15.2.0/bits/locale_classes.h \
+ /usr/include/c++/15.2.0/bits/locale_classes.tcc \
+ /usr/include/c++/15.2.0/system_error \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/error_constants.h \
+ /usr/include/c++/15.2.0/stdexcept /usr/include/c++/15.2.0/streambuf \
+ /usr/include/c++/15.2.0/bits/streambuf.tcc \
+ /usr/include/c++/15.2.0/bits/basic_ios.h \
+ /usr/include/c++/15.2.0/bits/locale_facets.h \
+ /usr/include/c++/15.2.0/cwctype /usr/include/wctype.h \
+ /usr/include/x86_64-linux-gnu/bits/wctype-wchar.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/ctype_base.h \
+ /usr/include/c++/15.2.0/bits/streambuf_iterator.h \
+ /usr/include/c++/15.2.0/x86_64-unknown-linux-gnu/bits/ctype_inline.h \
+ /usr/include/c++/15.2.0/bits/locale_facets.tcc \
+ /usr/include/c++/15.2.0/bits/basic_ios.tcc \
+ /usr/include/c++/15.2.0/bits/ostream.tcc /usr/include/c++/15.2.0/istream \
+ /usr/include/c++/15.2.0/bits/istream.tcc
